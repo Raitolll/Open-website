41e95c0581338aac14e8aeee076352cd73084469
Raitolll <uberfest91@gmail.com>
2024-11-02 17:31:52 +0100

    Add files via upload

---
+++ Open website.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+
+def url_open(url):
+    webbrowser.open(url)
+
+
+url_open("https://www.google.pl/?hl=pl")
+       #please write your website for example:https://www.google.pl/?hl=pl 
